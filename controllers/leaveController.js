24b6780f0fdb0e3a8bcddfa33c7602adc4530842
Shriramkhabale <sparkcomputer555@gmail.com>
2025-10-31 15:10:00 +0530

    leavecontroler chanegs

--- controllers/leaveController.js
+++ controllers/leaveController.js
@@ -52,8 +52,10 @@ exports.updateLeave = async (req, res) => {
     if (!leave) {
       return res.status(404).json({ message: 'Leave request not found' });
     }
+    console.log("req.user",req.user);
+    
     // Optional: Check if the user is authorized to update this leave (e.g., only the employee who created it)
-    if (leave.employee.toString() !== req.user.employeeId) {
+    if (leave.employee.toString() !== req.user.employeeId || req.user.userId) {
       return res.status(403).json({ message: 'Not authorized to update this leave request' });
     }
     // Optional: Prevent updates if status is not 'Pending'
